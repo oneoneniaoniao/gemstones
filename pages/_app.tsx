49ca96c3e2bdc4a58fb05dfc30c4a0e3977dd4c3
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2023-02-01 09:16:40 +0900

    [Add]MUI setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = createTheme({
+    palette: {
+      // mode: isDarkMode ?  "dark":"light",
+    },
+    typography: {
+      fontFamily: ["メイリオ", "Comic Sans MS"].join(","),
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
